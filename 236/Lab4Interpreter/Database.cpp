8727dc4eeb6d5e1eac96cd7de8f7a7efe0081430
sarraelayne <43146494+sarraelayne@users.noreply.github.com>
2019-04-16 15:30:30 -0600

    Delete Database.cpp

--- 236/Lab4Interpreter/Database.cpp
+++
@@ -1,9 +0,0 @@
-#include "Database.h"
-
-void Database::store(string name, Relation r) {
-	myMap.insert(pair<string, Relation>(name, r));
-}
-
-void Database::giveTuple(Tuple t, string name) {
-	//map.find(name)->second.tuples
-}
